e620d0a3d3fa19187aa11b7edbd361c8fed5eb99
MarkShevelev <shevelev_m@itstep.org>
2019-08-11 20:26:29 +0300

    ZFunc_tabulator template function is to construct  the plasma dispersion function table

---
+++ ZFuncTabulator/ZFunc.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef ZFunc_H
+#define ZFunc_H
+
+#include "Tabulator.h"
+#include "Runge.h"
+
+#include <vector>
+
+template <typename T>
+std::vector<T> ZFunc_tabulator(T darg, unsigned loop_count = 1u) {
+	using namespace std;
+	vector<T> table;
+	kahan_tabulator(
+		Runge4thOrder([](T arg, T val)->T { return -arg * val - 1.; })
+		, begin(table), end(table)
+		, T(0), T(0), darg
+		, loop_count
+	);
+	return table; //we count on NRVO
+}
+
+
+#endif
