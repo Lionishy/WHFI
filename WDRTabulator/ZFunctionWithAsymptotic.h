0bf35bd66eac4b258e651a60fb084af695ea05f4
Mark Shevelev <lionishy@gmail.com>
2019-08-12 19:31:55 +0300

    ZFuncWithAsymptotic parenthesis operator overload added

--- WDRTabulator/ZFunctionWithAsymptotic.h
+++ WDRTabulator/ZFunctionWithAsymptotic.h
@@ -6,16 +6,28 @@
 
 #include <vector>
 #include <memory>
+#include <cmath>
 
+/**
+ * Class represents plasma dispersion function (ZFunc) as a combination of the known values table and the asymptotic expression for the large values
+ */
 template <typename T>
 class ZFuncWithAsymptotic final {
 public:
 	T operator()(T arg) const {
-		
+		T farg = std::abs(arg);
+		unsigned idx = unsigned(farg / known_values->darg);
+		if (idx + 1u < known_values->table.size()) {
+			return T(arg >= 0. ? 1. : -1.) * ((known_values->table[idx + 1u] - known_values->table[idx]) / known_values->darg * (farg - darg * idx) + known_values->table[idx]);
+		}
+		else {
+			T over = T(1.) / ksi, square = over * over;
+			return -over * (T(1.) + square + T(3.) * square * square);
+		}
 	}
 
 private:
-	std::shared_ptr<StepArgumentTable<T>> known_values;
+	std::shared_ptr<StepArgumentTable<T> const> known_values;
 };
 
 #endif /*ZFunctionWithAsymptotic_H*/
