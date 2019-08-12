d8912cc1c6fc26f3fc69280602efca4ee8df28e4
Mark Shevelev <lionishy@gmail.com>
2019-08-12 21:09:15 +0300

    Table input/output error fixed: in 'read_table_binary' local vars changed to input argument fields

--- WDRTabulator/TableIO.h
+++ WDRTabulator/TableIO.h
@@ -93,7 +93,6 @@ std::istream& read_table_ascii(StepArgumentTable<ArgT, ValT> &table, std::istrea
 	unsigned int size;
 	ascii_in >> size;
 	table.table.resize(size);
-
 	for (auto &value : table.table)
 		ascii_in >> value;
 	return ascii_in;
@@ -124,9 +123,8 @@ std::istream& read_table_binary(StepArgumentTable<ArgT, ValT> &table, std::istre
 	unsigned int size;
 	ascii_in.read(reinterpret_cast<char*>(&size), sizeof(unsigned int));
 	table.table.resize(size);
-	ArgT arg0, darg;
-	ascii_in.read(reinterpret_cast<char*>(&arg0), sizeof(ArgT));
-	ascii_in.read(reinterpret_cast<char*>(&darg), sizeof(ArgT));
+	ascii_in.read(reinterpret_cast<char*>(std::addressof(table.arg0)), sizeof(ArgT));
+	ascii_in.read(reinterpret_cast<char*>(std::addressof(table.darg)), sizeof(ArgT));
 	for (auto &value : table.table)
 		ascii_in.read(reinterpret_cast<char*>(&value), sizeof(ValT));
 	return ascii_in;
