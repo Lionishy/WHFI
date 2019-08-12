5f0f15f11098551381480200346115b0d15352ca
MarkShevelev <shevelev_m@itstep.org>
2019-08-12 10:06:09 +0300

    Explicit type conversion added in Runge.h

--- ZFuncTabulator/Runge.h
+++ ZFuncTabulator/Runge.h
@@ -14,10 +14,10 @@ public:
 
 	ValT operator() (ArgT step, ArgT arg, ValT value) const {
 		ValT k1 = F(arg, value);
-		ValT k2 = F(arg + step / 2., value + k1 * step / 2.);
-		ValT k3 = F(arg + step / 2., value + k2 * step / 2.);
+		ValT k2 = F(arg + step / ValT(2.), value + k1 * step / ValT(2.));
+		ValT k3 = F(arg + step / ValT(2.), value + k2 * step / ValT(2.));
 		ValT k4 = F(arg + step, value + k2 * step); 
-		return step / 6. * (k1 + k2 + k3 + k4);
+		return step / ValT(6.) * (k1 + k2 + k3 + k4);
 	}
 
 private:
