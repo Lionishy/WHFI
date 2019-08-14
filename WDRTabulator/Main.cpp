e55a463fc4e8562d3568c4b1452fe5cbbc26c174
Mark Shevelev <lionishy@gmail.com>
2019-08-14 09:35:16 +0300

    ZFuncTabulator main function recovered

--- WDRTabulator/Main.cpp
+++ WDRTabulator/Main.cpp
@@ -1,12 +1,22 @@
 #include "ZFunctionWithAsymptotic.h"
 #include "Table.h"
 #include "TableIO.h"
+#include "LambdaR.h"
 
 #include <fstream>
 #include <iostream>
+#include <cmath>
 
 int main() {
 	using namespace std;
+
+
+	/*PhysicalParameters<float> params;
+	params.nc = 0.85f; params.nh = 0.15f; //nh+nc = 1.
+	params.betta_root_c = sqrt(0.5f); params.betta_root_h = sqrt(10.f)*params.betta_root_c; //Tc/Th = 0.1
+	params.bulk_to_term_c = -1.f/params.betta_root_c * sqrt(1.f/1836.f);
+	params.bulk_to_term_h = (params.nc / params.nh) * params.bulk_to_term_c * sqrt(0.1f); //Tc/Th = 0.1
+
 	auto fTable_ptr = make_shared<StepArgumentTable<float, float>>();
 	{
 		ifstream zfunc_table_in;
@@ -15,20 +25,16 @@ int main() {
 		zfunc_table_in.open("./fZfunc.tbl",ios::in|ios::binary);
 		read_table_binary(*fTable_ptr, zfunc_table_in);
 	}
-	ZFuncWithAsymptotic<float> fZFunc(fTable_ptr);
 
-	{
-		ArgValueTable<float, float> newTable;
-		for (float arg = 0.; arg < 100.f; arg += 7.e-3f)
-			newTable.table.push_back({ arg,fZFunc(arg) });
-
-		ofstream zfunc_table_out;
-		zfunc_table_out.exceptions(ios::badbit | ios::failbit);
-
-		zfunc_table_out.open("./fZfunc.txt");
-		write_table_ascii(newTable, zfunc_table_out);
-	}
-
+	auto LR = make_lambdar(ZFuncWithAsymptotic<float>(fTable_ptr), params, -0.34f);
+	unsigned step_count = 0; float step = 1.e-4f, LRcurr = LR(0.f), LRnext = LRcurr;
+	do {
+		LRnext = LR(step * ++step_count);
+		if (LRcurr * LRnext <= 0.f) break;
+		LRcurr = LRnext;
+	} while (step * step_count < 1.f);
+	cout << step * (step_count - 1u) << ' ' << step * step_count << endl;
+	*/
 
 	return 0;
 }
