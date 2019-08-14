5e653931a403728eb9e29f6c248db529431d7521
Mark Shevelev <lionishy@gmail.com>
2019-08-14 18:56:10 +0300

    Calculation of the real part of the whistler disperstion relation has been added

--- WDRTabulator/Main.cpp
+++ WDRTabulator/Main.cpp
@@ -6,35 +6,88 @@
 #include <fstream>
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <future>
+#include <iomanip>
 
-int main() {
+template <typename T>
+std::vector<T> generate_grid(T begin, T end, T step) {
 	using namespace std;
+	vector<T> grid; grid.reserve(unsigned((end - begin) / step));
+	unsigned step_counter = 0; auto arg = begin;
+	while (arg <= end) {
+		if (abs(arg) > T(1.e-7)) {
+			grid.push_back(arg);
+		}
+		arg = step * ++step_counter + begin;
+	}
+	return grid;
+}
 
+template <typename T, typename FunT>
+T step_solver(FunT F, T from, T to, T step) {
+	unsigned step_count = 0; T curr = F(from), next = curr;
+	do {
+		next = F(step * ++step_count);
+		if (curr * next <= T(0.))
+			return 0.5 * (step * (step_count - 1u) + step * step_count);
+		curr = next;
+	} while (step * step_count < to);
+	return T(std::nan(""));
+}
 
-	/*PhysicalParameters<float> params;
-	params.nc = 0.85f; params.nh = 0.15f; //nh+nc = 1.
-	params.betta_root_c = sqrt(0.5f); params.betta_root_h = sqrt(10.f)*params.betta_root_c; //Tc/Th = 0.1
-	params.bulk_to_term_c = -1.f/params.betta_root_c * sqrt(1.f/1836.f);
-	params.bulk_to_term_h = (params.nc / params.nh) * params.bulk_to_term_c * sqrt(0.1f); //Tc/Th = 0.1
-
-	auto fTable_ptr = make_shared<StepArgumentTable<float, float>>();
+template <typename T>
+ArgValueTable<T, T> real_part_wdr(std::string ZFuncTableFilename, PhysicalParameters<T> params, std::vector<T> k_grid = generate_grid<T>(T(-0.7),T(0.7),T(1.e-3))) {
+	using namespace std;
+	auto table_ptr = make_shared<StepArgumentTable<T,T>>();
 	{
 		ifstream zfunc_table_in;
 		zfunc_table_in.exceptions(ios::badbit | ios::failbit);
 
-		zfunc_table_in.open("./fZfunc.tbl",ios::in|ios::binary);
-		read_table_binary(*fTable_ptr, zfunc_table_in);
+		zfunc_table_in.open(ZFuncTableFilename, ios::in | ios::binary);
+		read_table_binary(*table_ptr, zfunc_table_in);
 	}
+	
+	vector<pair<T,future<T>>> omegas;
+	auto kernel = [table_ptr, params](T k) {
+		return step_solver(make_lambdar(ZFuncWithAsymptotic<T>(table_ptr), params, k), T(0.), T(1.), T(1.e-4));
+	};
+	for (auto k : k_grid)
+		omegas.push_back({ k, async(launch::async,kernel,k) });
 
-	auto LR = make_lambdar(ZFuncWithAsymptotic<float>(fTable_ptr), params, -0.34f);
-	unsigned step_count = 0; float step = 1.e-4f, LRcurr = LR(0.f), LRnext = LRcurr;
-	do {
-		LRnext = LR(step * ++step_count);
-		if (LRcurr * LRnext <= 0.f) break;
-		LRcurr = LRnext;
-	} while (step * step_count < 1.f);
-	cout << step * (step_count - 1u) << ' ' << step * step_count << endl;
-	*/
+	ArgValueTable<T, T> result_table;
+	for (auto& f : omegas)
+		result_table.table.push_back({ f.first,f.second.get() });
+	return result_table;
+}
+
+/**
+ * Function to ease construction of the PhysicalParameters struct from a lesser number of the input values
+ * @param nc - share of the core particles
+ * @param betta_c - core part betta (pressure ratio)
+ * @param TcTh_ratio - ratio of the core temperature to the halo temperature
+ * @param bulk_velocity - core bulk velocity in terms of Alfven speed
+ */
+template <typename T>
+PhysicalParameters<T> calculate_parameters(T nc, T betta_c, T TcTh_ratio, T bulk_velocity) {
+	PhysicalParameters<T> params;
+	params.nc = nc; params.nh = T(1.) - nc; //nc + nh = 1.
+	params.betta_root_c = sqrt(T(0.5) * betta_c); 
+	params.betta_root_h = sqrt(T(0.5) * betta_c / TcTh_ratio);
+	params.bulk_to_term_c = bulk_velocity / params.betta_root_c * sqrt(T(1. / 1836.));
+	params.bulk_to_term_h = -(params.nc / params.nh) * params.bulk_to_term_c * sqrt(TcTh_ratio);
+	return params;
+}
+
+int main() {
+	using namespace std;
+	auto k_omega_table = real_part_wdr("./dZFunc.tbl", calculate_parameters(0.85, 1., 0.1, -3.));
+	{
+		ofstream wdr("./dwdr-3.txt"); wdr << setprecision(8) << fixed;
+		write_table_ascii(k_omega_table, wdr);
+	}
 
 	return 0;
 }
