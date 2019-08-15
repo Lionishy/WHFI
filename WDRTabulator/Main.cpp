e362b981d309884345e1c8d9beb78295eaccee01
Lionishy <lionishy@gmail.com>
2019-08-15 14:49:52 +0300

    Construction of the k-omega and the k-derivative grids redesigned

--- WDRTabulator/Main.cpp
+++ WDRTabulator/Main.cpp
@@ -13,6 +13,9 @@
 #include <future>
 #include <iomanip>
 
+/**
+ * Function to generate a wave number gring, a number of k values
+ */
 template <typename T>
 std::vector<T> generate_grid(T begin, T end, T step) {
 	using namespace std;
@@ -27,6 +30,13 @@ std::vector<T> generate_grid(T begin, T end, T step) {
 	return grid;
 }
 
+/**
+ * Primitive solver to find real roots
+ * @param F    - function to find root of
+ * @param from - the first value of the argument segment to find root in
+ * @param to   - the last value of the argument segment to find root in
+ * @param step - difference between two adjacent values to examine
+ */
 template <typename T, typename FunT>
 T step_solver(FunT F, T from, T to, T step) {
 	unsigned step_count = 0; T curr = F(from), next = curr;
@@ -39,10 +49,13 @@ T step_solver(FunT F, T from, T to, T step) {
 	return T(std::nan(""));
 }
 
+/**
+ * Function to fast create a callable representing the plasma dispersion function (ZFunc) from precalculated values stored in a file
+ */
 template <typename T>
-ArgValueTable<T, T> real_part_wdr(std::string ZFuncTableFilename, PhysicalParameters<T> params, std::vector<T> k_grid = generate_grid<T>(T(-0.7),T(0.7),T(1.e-3))) {
+auto make_ZFunc_from_file(std::string ZFuncTableFilename) {
 	using namespace std;
-	auto table_ptr = make_shared<StepArgumentTable<T,T>>();
+	auto table_ptr = make_shared<StepArgumentTable<T, T>>();
 	{
 		ifstream zfunc_table_in;
 		zfunc_table_in.exceptions(ios::badbit | ios::failbit);
@@ -50,10 +63,18 @@ ArgValueTable<T, T> real_part_wdr(std::string ZFuncTableFilename, PhysicalParame
 		zfunc_table_in.open(ZFuncTableFilename, ios::in | ios::binary);
 		read_table_binary(*table_ptr, zfunc_table_in);
 	}
-	
+	return ZFuncWithAsymptotic<T>(table_ptr);
+}
+
+/**
+ * Function to counstruct a table representing the real part of the whistler dispersion relation 
+ */
+template <typename T, typename FunT>
+ArgValueTable<T, T> real_part_wdr(FunT Z, PhysicalParameters<T> params, std::vector<T> k_grid = generate_grid<T>(T(-0.7),T(0.7),T(1.e-3))) {
+	using namespace std;
 	vector<pair<T,future<T>>> omegas;
-	auto kernel = [table_ptr, params](T k) {
-		return step_solver(make_lambdar(ZFuncWithAsymptotic<T>(table_ptr), params, k), T(0.), T(1.), T(1.e-4));
+	auto kernel = [Z, params](T k) {
+		return step_solver(LambdaR<T,FunT>(Z, params, k), T(0.), T(1.), T(1.e-4));
 	};
 	for (auto k : k_grid)
 		omegas.push_back({ k, async(launch::async,kernel,k) });
@@ -64,22 +85,14 @@ ArgValueTable<T, T> real_part_wdr(std::string ZFuncTableFilename, PhysicalParame
 	return result_table;
 }
 
-template <typename T>
-ArgValueTable<T, T> omega_to_derivative_transform(std::string ZFuncTableFilename, ArgValueTable<T, T> const &k_omega_table, PhysicalParameters<T> p) {
+/**
+ * Function to transform known values of the (k,omega) pairs to the (k,LambdaR derivative) pairs table
+ */
+template <typename T, typename FunT>
+ArgValueTable<T, T> omega_to_derivative_transform(FunT Z, ArgValueTable<T, T> const &k_omega_table, PhysicalParameters<T> p) {
 	using namespace std;
 	ArgValueTable<T, T> k_derivative;
 	k_derivative.table.resize(k_omega_table.table.size());
-
-	auto table_ptr = make_shared<StepArgumentTable<T, T>>();
-	{
-		ifstream zfunc_table_in;
-		zfunc_table_in.exceptions(ios::badbit | ios::failbit);
-
-		zfunc_table_in.open(ZFuncTableFilename, ios::in | ios::binary);
-		read_table_binary(*table_ptr, zfunc_table_in);
-	}
-
-	ZFuncWithAsymptotic<T> Z(table_ptr);
 	transform(begin(k_omega_table.table), end(k_omega_table.table), begin(k_derivative.table), 
 		[Z,p](auto& k_omega) {
 			return pair<T,T>( k_omega.first, make_lambdar_rootderivative(Z,p,k_omega.first)(k_omega.second) );
@@ -109,15 +122,17 @@ PhysicalParameters<T> calculate_parameters(T nc, T betta_c, T TcTh_ratio, T bulk
 int main() {
 	using namespace std;
 	try {
-		auto p = calculate_parameters(0.85f, 1.f / 0.85f, 0.1f, -1.f);
-		auto k_omega_table = real_part_wdr("./fZFunc.tbl", p);
-		auto k_derivative_table = omega_to_derivative_transform("./fZFunc.tbl", k_omega_table, p);
+		auto p = calculate_parameters(0.85f, 1.f / 0.85f, 0.1f, -15.f);
+		auto Z = make_ZFunc_from_file<float>("./fZFunc.tbl");
+
+		auto k_omega_table = real_part_wdr(Z, p);
+		auto k_derivative_table = omega_to_derivative_transform(Z, k_omega_table, p);
 		{
-			ofstream wdr("./fwdr-1.txt"); wdr << setprecision(8) << fixed;
+			ofstream wdr("./fwdr-15.txt"); wdr << setprecision(8) << fixed;
 			write_table_ascii(k_omega_table, wdr);
 		}
 		{
-			ofstream wdr_derivative("./fwdr_derive-1.txt"); wdr_derivative << setprecision(8) << fixed;
+			ofstream wdr_derivative("./fwdr_derive-15.txt"); wdr_derivative << setprecision(8) << fixed;
 			write_table_ascii(k_derivative_table, wdr_derivative);
 		}
 	}
