c5c6cfb145da3dbeb0e469bab2be9e35958ca289
MarkShevelev <shevelev_m@itstep.org>
2019-08-15 12:52:24 +0300

    LambdaR derivative at root points has been added

---
+++ WDRTabulator/LambdaRRootDerivative.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef LambdaRRootDerivative_H
+#define LambdaRRootDerivative_H
+
+#include "PhysicalParameters.h"
+
+template <typename T, typename FunT>
+class LambdaRRootDerivative final {
+public:
+	LambdaRRootDerivative(FunT Z, PhysicalParameters<T> p, T k): Z(Z), p(p), k(k) { }
+	T operator()(T omega) const {
+		T arg_c = (omega - T(1.)) / (k * p.betta_root_c) - p.bulk_to_term_c;
+		T arg_h = (omega - T(1.)) / (k * p.betta_root_h) - p.bulk_to_term_h;
+		T Zc = Z(arg_c), Zh = Z(arg_h);
+		return p.nc / (k * p.betta_root_c) * (-Zc + (omega / (k * p.betta_root_c) - p.bulk_to_term_c) * (Zc * arg_c + T(1.)))
+			+ p.nh / (k * p.betta_root_h) * (-Zh + (omega / (k * p.betta_root_h) - p.bulk_to_term_h) * (Zh * arg_h + T(1.)));
+	}
+
+private:
+	FunT Z;
+	PhysicalParameters<T> p;
+	T k;
+};
+
+template <typename T, typename FunT>
+auto make_lambdar_rootderivative(FunT Z, PhysicalParameters<T> p, T k) {
+	return LambdaRRootDerivative(Z, p, k);
+}
+
+#endif /*LambdaRRootDerivative*/
--- WDRTabulator/Main.cpp
+++ WDRTabulator/Main.cpp
@@ -2,6 +2,7 @@
 #include "Table.h"
 #include "TableIO.h"
 #include "LambdaR.h"
+#include "LambdaRRootDerivative.h"
 
 #include <fstream>
 #include <iostream>
@@ -32,7 +33,7 @@ T step_solver(FunT F, T from, T to, T step) {
 	do {
 		next = F(step * ++step_count);
 		if (curr * next <= T(0.))
-			return 0.5 * (step * (step_count - 1u) + step * step_count);
+			return T(0.5) * (step * (step_count - 1u) + step * step_count);
 		curr = next;
 	} while (step * step_count < to);
 	return T(std::nan(""));
@@ -63,6 +64,30 @@ ArgValueTable<T, T> real_part_wdr(std::string ZFuncTableFilename, PhysicalParame
 	return result_table;
 }
 
+template <typename T>
+ArgValueTable<T, T> omega_to_derivative_transform(std::string ZFuncTableFilename, ArgValueTable<T, T> const &k_omega_table, PhysicalParameters<T> p) {
+	using namespace std;
+	ArgValueTable<T, T> k_derivative;
+	k_derivative.table.resize(k_omega_table.table.size());
+
+	auto table_ptr = make_shared<StepArgumentTable<T, T>>();
+	{
+		ifstream zfunc_table_in;
+		zfunc_table_in.exceptions(ios::badbit | ios::failbit);
+
+		zfunc_table_in.open(ZFuncTableFilename, ios::in | ios::binary);
+		read_table_binary(*table_ptr, zfunc_table_in);
+	}
+
+	ZFuncWithAsymptotic<T> Z(table_ptr);
+	transform(begin(k_omega_table.table), end(k_omega_table.table), begin(k_derivative.table), 
+		[Z,p](auto& k_omega) {
+			return pair<T,T>( k_omega.first, make_lambdar_rootderivative(Z,p,k_omega.first)(k_omega.second) );
+		}
+	);
+	return k_derivative;
+}
+
 /**
  * Function to ease construction of the PhysicalParameters struct from a lesser number of the input values
  * @param nc - share of the core particles
@@ -83,11 +108,24 @@ PhysicalParameters<T> calculate_parameters(T nc, T betta_c, T TcTh_ratio, T bulk
 
 int main() {
 	using namespace std;
-	auto k_omega_table = real_part_wdr("./fZFunc.tbl", calculate_parameters(0.85f, 1.f/0.85f, 0.1f, -1.f));
-	{
-		ofstream wdr("./fwdr-7.txt"); wdr << setprecision(8) << fixed;
-		write_table_ascii(k_omega_table, wdr);
+	try {
+		auto p = calculate_parameters(0.85f, 1.f / 0.85f, 0.1f, -1.f);
+		auto k_omega_table = real_part_wdr("./fZFunc.tbl", p);
+		auto k_derivative_table = omega_to_derivative_transform("./fZFunc.tbl", k_omega_table, p);
+		{
+			ofstream wdr("./fwdr-1.txt"); wdr << setprecision(8) << fixed;
+			write_table_ascii(k_omega_table, wdr);
+		}
+		{
+			ofstream wdr_derivative("./fwdr_derive-1.txt"); wdr_derivative << setprecision(8) << fixed;
+			write_table_ascii(k_derivative_table, wdr_derivative);
+		}
 	}
+	catch (exception const& ex) {
+		cout << ex.what() << endl;
+	}
+
+
 
 	return 0;
 }
